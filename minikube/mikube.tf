a4f7345ea60309c50d6ddbc96a35a48314884719
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-05-27 15:14:46 +0530

    new kube

---
+++ minikube/mikube.tf
@@ -0,0 +1,23 @@
+module "minikube" {
+  source = "github.com/scholzj/terraform-aws-minikube"
+
+  aws_region    = "us-east-1"
+  cluster_name  = "roboshop"
+  aws_instance_type = "t3.medium"
+  ssh_public_key = "~/.ssh/minikube.pub"
+  aws_subnet_id = "subnet-043091cc04d94c6a7"
+  ami_image_id = "ami-0772a17d6f58a2754"
+  hosted_zone = "bigmatrix.in"
+  hosted_zone_private = false
+
+  tags = {
+    Application = "Minikube"
+  }
+
+  addons = [
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/storage-class.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/heapster.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/dashboard.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns.yaml"
+  ]
+}
---
+++ minikube/workstation.tf
@@ -0,0 +1,63 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "workstation"
+  ami = data.aws_ami.centos8.id
+  instance_type          = "t2.micro"
+#   key_name               = "user1"
+#   monitoring             = true
+  vpc_security_group_ids = [aws_security_group.allow_minikube.id]
+  subnet_id              = "subnet-043091cc04d94c6a7"
+  user_data = file("docker.sh")
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+
+resource "aws_security_group" "allow_minikube" {
+  name        = "allow_tls"
+  description = "created for minikube"
+  
+
+  tags = {
+    Name = "allow_minikube"
+  }
+
+  ingress {
+   description = "HTTPS ingress"
+   from_port   = 0
+   to_port     = 0
+   protocol    = "-1"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+
+egress {
+   from_port   = 0
+   to_port     = 0
+   protocol    = "-1"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+}
+
+data "aws_ami" "centos8" {
+owners           = ["973714476881"]
+most_recent      = true
+
+    filter {
+        name   = "name"
+        values = ["Centos-8-DevOps-Practice"]
+    }
+
+    #  filter {
+    #      name   = "root-device-type"
+    #      values = ["EBS"]
+    # }
+
+    #  filter {
+    #      name   = "virtualization-type"
+    #      values = ["hvm"]
+    #  }
+
+}
